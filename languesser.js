9aa8657bcb05191a51695e1fda81ef4f08391b6a
palant-dev <antonio.palomba2015@gmail.com>
2024-07-20 12:54:23 +0200

    Adds an error if the language is not detect and adds colors to the prints

--- languesser.js
+++ languesser.js
@@ -1,13 +1,19 @@
 var franc = require('./node_modules/franc');
 var langs = require('./node_modules/langs');
+var colors = require('./node_modules/colors')
 
 const argv = process.argv.slice(2);
 const stringArgv = argv.join(" ");
-
-
 const codeDetLang = franc(stringArgv);
-console.log(`Language code detected:`, codeDetLang);
 
-const nameDetLang = langs.where("2", codeDetLang);
+if (codeDetLang !== "und") {
+    console.log(colors.blue(`Language code detected:`, codeDetLang));
 
-console.log('The language is:', nameDetLang.name);
+    const nameDetLang = langs.where("2", codeDetLang);
+
+    console.log(colors.blue('The language is:', nameDetLang.name));
+} else {
+    console.log(colors.red('Impossible to detect the language, try with a simpler sentence.'));
+}
+
+
